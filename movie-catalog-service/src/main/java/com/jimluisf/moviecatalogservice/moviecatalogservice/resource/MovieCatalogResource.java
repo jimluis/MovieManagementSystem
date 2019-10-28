f2e47b7298e2ef87323eedcd54ecfa4b9477ecc8
Luis Felipe Jimenez <jimluisf@gmail.com>
2019-10-28 13:02:04 -0400

    Adding microservice communication

--- movie-catalog-service/src/main/java/com/jimluisf/moviecatalogservice/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/jimluisf/moviecatalogservice/moviecatalogservice/resource/MovieCatalogResource.java
@@ -1,23 +1,71 @@
 package com.jimluisf.moviecatalogservice.moviecatalogservice.resource;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.assertj.core.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.jimluisf.moviecatalogservice.moviecatalogservice.models.CatalogItem;
+import com.jimluisf.moviecatalogservice.moviecatalogservice.models.Movie;
+import com.jimluisf.moviecatalogservice.moviecatalogservice.models.Rating;
+import com.jimluisf.moviecatalogservice.moviecatalogservice.models.UserRating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource 
 {
-
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private WebClient.Builder  webClientBuilder;
+	
 		@GetMapping("/{userId}")
 		public List<CatalogItem> getCatalog(@PathVariable String userId)
 		{
-			return Collections.singletonList(new CatalogItem("Transformers", "Test", 4));
+			List<CatalogItem> catalogItemList = new ArrayList<CatalogItem>();
+
+			UserRating userRating = restTemplate.getForObject("http://localhost:8083/ratingdata/users/"+userId, UserRating.class);
+			
+			
+			for (Rating rating2 : userRating.getUserRating()) 
+			{
+				//for each movieId call movie into service and get details
+				Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating2.getMovieId(), Movie.class);
+				
+				//put them all together
+				catalogItemList.add(new CatalogItem(movie.getName(), "Desc", rating2.getRating()));
+			}
+			
+			return catalogItemList;
+
+			
 		}
 }
+
+
+//return ratings.stream().map(rating -> {
+//Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+
+//Movie movie = webClientBuilder.build()
+//.get()//get method, if post is needed call post()
+//.uri("http://localhost:8082/movies/"+rating.getMovieId())
+//.retrieve()
+//.bodyToMono(Movie.class)//This is a promise
+//.block(); // block waits until the promise returns
+
+//return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+//})
+//
+//.collect(Collectors.toList());
+
+
--- movie-rating-service/src/main/java/com/jimluisf/movieratingservice/movieratingservice/resource/RatingResource.java
+++ movie-rating-service/src/main/java/com/jimluisf/movieratingservice/movieratingservice/resource/RatingResource.java
@@ -1,11 +1,16 @@
 package com.jimluisf.movieratingservice.movieratingservice.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jimluisf.movieratingservice.movieratingservice.model.Rating;
+import com.jimluisf.movieratingservice.movieratingservice.model.UserRating;
 
 @RestController
 @RequestMapping("/ratingdata")
@@ -16,5 +21,18 @@ public class RatingResource
 	public Rating getRating(@PathVariable String movieId)
 	{
 		return new Rating(movieId, 4);
+	}
+	
+	
+	@GetMapping("/users/{userId}")
+	public UserRating getRatingList(@PathVariable String userId)
+	{
+		List<Rating> ratingList = new ArrayList<Rating>();
+		ratingList.add(new Rating("1234", 4));
+		ratingList.add(new Rating("5678", 3));
+		
+		UserRating userRating = new UserRating();
+		userRating.setUserRating(ratingList);
+		return userRating;
 	}
 }
